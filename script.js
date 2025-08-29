5aac96c7385b6b1946f92a724075b0edf3bb26e2
Seanjohnferesi <seanjohnferesi27@gmail.com>
2025-08-29 14:30:06 +0200

    add prompts for task 1 and task 2 input (title, description, status)

---
+++ script.js
@@ -0,0 +1,20 @@
+//Task 1
+const task1Title = prompt("Enter task 1 title");
+console.log(`Title: ${task1Title}`);
+
+const task1Description = prompt("Enter task 1 description");
+console.log(`Description: ${task1Description}`);
+
+const task1Status = prompt("Enter task 1 status");
+console.log(`Status: ${task1Status}`);
+
+//Task 2
+const task2Title = prompt("Enter task 2 title");
+console.log(`Title: ${task1Title}`);
+
+const task2Description = prompt("Enter task 2 description");
+console.log(`Description: ${task1Description}`);
+
+const task2Status = prompt("Enter task 2 status");
+console.log(`Status: ${task1Status}`);
+
